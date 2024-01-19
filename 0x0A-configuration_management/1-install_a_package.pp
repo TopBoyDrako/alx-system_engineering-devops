20f1b5b34b6ac0c3821fb219eb6b73ddb8147f82
TopBoyDrako <sinbadprince9@gmail.com>
2024-01-20 00:24:20 +0100

    configuration management

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# I installed flask from pip3 using Puppet
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# lii the process killmenow
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
